85d2649570c708fc6175306e7728b11a6b2bb40b
Shayan <shayanhorbank@gmail.com>
2025-05-27 14:39:38 +0200

    Add destination route to QR code redirects

---
+++ src/data/qrRedirects.ts
@@ -0,0 +1,11 @@
+const qrRedirects: Record<string, string> = {
+  '/GPHGS1': '/exhibition/schwelle',
+  '/GSP7WP': '/exhibition/wirrnis',
+  '/H7BVJL': '/exhibition/hingabe',
+  '/JWGQ9K': '/assign-page',
+  '/RY1EXM': '/guestbook/new',
+  '/RX8PXF': '/assign-page',
+  '/X2U73Q': '/assign-page',
+};
+
+export default qrRedirects;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,5 @@
 import { defineMiddleware } from 'astro:middleware';
-
-const qrRedirects: Record<string, string> = {
-  '/X2U73Q': '/assign-page',
-  '/GPHGS1': '/assign-page',
-  '/RY1EXM': '/assign-page',
-  '/JWGQ9K': '/assign-page',
-  '/GSP7WP': '/assign-page',
-  '/RX8PXF': '/assign-page',
-  '/H7BVJL': '/assign-page',
-};
+import qrRedirects from './data/qrRedirects';
 
 export const onRequest = defineMiddleware(async ({ url, preferredLocale, redirect }, next) => {
   const pathname = url.pathname;

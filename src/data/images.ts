1d33d2debbbeb8130e8c04ffc0354ff39312c997
Shayan <shayanhorbank@gmail.com>
2025-05-28 10:20:01 +0200

    Add first 8 images

--- src/data/images.ts
+++ src/data/images.ts
@@ -6,18 +6,36 @@ export type ImageEntry = {
 
 export const images: ImageEntry[] = [
   {
-    src: "/images/haus.png",
-    alt: "Haus",
+    src: "/images/posters/Poster_Englisch_All-1.webp",
+    alt: "Poster",
     eager: true,
   },
   {
-    src: "/images/haus.png",
-    alt: "Haus",
+    src: "/images/posters/Poster_Englisch_All-2.webp",
+    alt: "Poster",
   },
   {
-    src: "/images/haus.png",
-    alt: "Haus",
+    src: "/images/posters/Poster_Englisch_All-3.webp",
+    alt: "Poster",
   },
-
-
+  {
+    src: "/images/posters/Poster_Englisch_All-4.webp",
+    alt: "Poster",
+  },
+  {
+    src: "/images/posters/Poster_Englisch_All-5.webp",
+    alt: "Poster",
+  },
+  {
+    src: "/images/posters/Poster_Englisch_All-6.webp",
+    alt: "Poster",
+  },
+  {
+    src: "/images/posters/Poster_Englisch_All-7.webp",
+    alt: "Poster",
+  },
+  {
+    src: "/images/posters/Poster_Englisch_All-8.webp",
+    alt: "Poster",
+  },
 ];

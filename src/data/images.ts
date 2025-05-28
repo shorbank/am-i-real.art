4eeb7f4289ff626b75f0ed8c3fd58039b317b120
Shayan <shayanhorbank@gmail.com>
2025-05-28 10:01:23 +0200

    Add: translations page + image loading logic

---
+++ src/data/images.ts
@@ -0,0 +1,23 @@
+export type ImageEntry = {
+  src: string;
+  alt: string;
+  eager?: boolean;
+};
+
+export const images: ImageEntry[] = [
+  {
+    src: "/images/haus.png",
+    alt: "Haus",
+    eager: true,
+  },
+  {
+    src: "/images/haus.png",
+    alt: "Haus",
+  },
+  {
+    src: "/images/haus.png",
+    alt: "Haus",
+  },
+
+
+];
--- src/data/qrRedirects.ts
+++ src/data/qrRedirects.ts
@@ -2,7 +2,7 @@ const qrRedirects: Record<string, string> = {
   '/GPHGS1': '/exhibition/schwelle',
   '/GSP7WP': '/exhibition/wirrnis',
   '/H7BVJL': '/exhibition/hingabe',
-  '/JWGQ9K': '/assign-page',
+  '/JWGQ9K': '/translations',
   '/RY1EXM': '/guestbook/new',
   '/RX8PXF': '/assign-page',
   '/X2U73Q': '/assign-page',
